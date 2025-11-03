573af4fe260c5b34d000f196c94ff27064d04352
thorbengrosser <github@thorben.io>
2025-11-03 13:01:08 +0100

    Flask fix

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ import os
 from urllib.parse import urljoin
 from pathlib import Path
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 CORS(app, resources={r"/*": {"origins": Config.CORS_ORIGINS}})
 socketio = SocketIO(
